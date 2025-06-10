6a10b501f507fd3eb18cf5c8e3021a05cf88270a
Cristobal Rodriguez <cristobal.rod0907@gmail.com>
2025-06-09 23:00:49 -0600

    feat: add input table

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,4 +1,5 @@
 #include <imgui.h>
+#include <cstdio>
 
 #include "utils/sudoku.h"
 
@@ -16,6 +17,26 @@ void RenderTable(Table table) {
   }
 }
 
+void RenderInput(AppState &state) {
+  if (!ImGui::BeginChild("Input Table", ImVec2(0, 0), true))
+    return;
+  ImGui::Text("Establece los valores iniciales de la tabla.");
+  ImGui::SeparatorText("Sudoku");
+  if (!ImGui::BeginTable("Input Table", TABLE_SIZE)) return;
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    ImGui::TableNextRow();
+    for (int j = 0; j < TABLE_SIZE; j++) {
+      char label[32];
+      ImGui::TableSetColumnIndex(j);
+      snprintf(label, sizeof(label), "##cell_%d_%d", i, j);
+      ImGui::SliderInt(label, &state.table[i][j], 0, TABLE_SIZE);
+    }
+  }
+  ImGui::EndTable();
+  if (ImGui::Button("Aceptar")) state.status = Resolving;
+  ImGui::EndChild();
+}
+
 void RenderUi(AppState *state) {
   const ImGuiViewport *main_viewport = ImGui::GetMainViewport();
   ImGui::SetNextWindowPos(
@@ -24,7 +45,16 @@ void RenderUi(AppState *state) {
       ImVec2(main_viewport->WorkSize.x, main_viewport->WorkSize.y));
   
   if(ImGui::Begin("Sudoku Solver", NULL, ImGuiWindowFlags_NoDecoration)) {
-    RenderTable(state->table);
+    switch (state->status) {
+      case Resolving:
+        RenderTable(state->table);
+        break;
+      case Selecting:
+        RenderInput(*state);
+        break;
+      case Finished:
+        break;
+    }
     ImGui::End();
   }
 }
